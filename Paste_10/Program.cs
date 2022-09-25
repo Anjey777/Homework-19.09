590db3479abf30b08871a2144d8b12079b1fa997
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2022-09-25 12:31:45 +0300

    Paste_10 finish

--- Paste_10/Program.cs
+++ Paste_10/Program.cs
@@ -4,3 +4,24 @@
 782 -> 8
 918 -> 1
 */
+
+int GetRandomNumber(int leftRange, int rightRange) // [100-1000)
+{
+    Random rnd = new Random();
+    int result = rnd.Next(leftRange, rightRange); // [leftRange,rightRange)
+    return result;
+}
+
+int DeleteSecondDigit(int number)
+{
+    int firstDigit = number / 100;
+    int thirdDigit = number % 10;
+
+    return firstDigit*10 + thirdDigit;
+}
+
+int number = GetRandomNumber(100,1000);
+Console.WriteLine(number);
+
+int twoDigits = DeleteSecondDigit(number);
+Console.WriteLine(twoDigits);

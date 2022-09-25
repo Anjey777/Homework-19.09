c42b99166d3ca03582fbbcae31999fd311aaf76c
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2022-09-25 18:34:21 +0300

    Paste_13 finish

--- Paste_13/Program.cs
+++ Paste_13/Program.cs
@@ -4,3 +4,14 @@
 78 -> третьей цифры нет
 32679 -> 6
 */
+
+Console.Write("Введи число:");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2)
+
+  Console.WriteLine("третья цифра " + anyNumberText[2]);
+
+else 
+
+  Console.WriteLine("третьей цифры нет");

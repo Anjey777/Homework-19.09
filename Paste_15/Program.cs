116fc874aa6279af3360e831a5d2f6b4bab603f7
Anjey777 <113132641+Anjey777@users.noreply.github.com>
2022-09-25 19:24:33 +0300

    Paste_14 finish

--- Paste_15/Program.cs
+++ Paste_15/Program.cs
@@ -4,3 +4,15 @@
 7 -> да
 1 -> нет
 */
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int daynumber = Convert.ToInt32 (Console.ReadLine());
+if ((daynumber ==6) || (daynumber ==7))
+{
+Console.WriteLine(("(Это выходной день) - ДА"));
+}
+else if ((daynumber<1) || (daynumber>7))
+{
+Console.WriteLine("Вы ошиблись, в неделе 7 дней");
+}
+else Console.WriteLine("(Этот день будний) - НЕТ");
